92531475934cf34db6470ad612b329cb13307dd9
jamjavascript <129390417+jamjavascript@users.noreply.github.com>
2025-02-12 16:43:11 -0700

    Add files via upload

---
+++ src/routes/reminders/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    /** @type {{ text: string }[]} */
+    let reminders = [];
+  
+    async function fetchReminders() {
+      // Fetch reminders from the database
+      const response = await fetch('/api/reminders');
+      reminders = await response.json();
+    }
+  
+    fetchReminders();
+  </script>
+  
+  <div>
+    <h1>Reminders</h1>
+    <ul>
+      {#each reminders as reminder}
+        <li>{reminder.text}</li>
+      {/each}
+    </ul>
+  </div>
+  
